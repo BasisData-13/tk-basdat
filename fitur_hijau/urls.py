1c6da6c16b1e27e1ddaf5aad5b29e94f12a86345
Vincent Suhardi <vinjupt@gmail.com>
2024-05-17 01:11:36 +0700

    progress crud_kelola_playlist

--- fitur_hijau/urls.py
+++ fitur_hijau/urls.py
@@ -6,9 +6,12 @@ app_name = 'fitur_hijau'
 urlpatterns = [
     # CRUD KELOLA PLAYLIST
     path('crud_kelola_playlist_main/', kelola_user_playlist_main, name='crud_kelola_playlist_main'),
-    path('crud_kelola_playlist_detail/', show_crud_kelola_playlist_detail, name='crud_kelola_playlist_detail'),
-    path('crud_kelola_playlist_tambah_lagu', tambah_lagu, name='crud_kelola_playlist_tambah_lagu'),
-    path('crud_kelola_playlist_tambah_playlist', tambah_playlist, name='crud_kelola_playlist_tambah_playlist'),
+    path('crud_kelola_playlist_tambah_lagu/', tambah_lagu, name='crud_kelola_playlist_tambah_lagu'),
+    path('tambah_playlist', tambah_playlist, name='tambah_playlist'),
+    path('hapus_playlist/<uuid:playlist_id>/', hapus_playlist, name='hapus_playlist'),
+    path('detail_playlist/<uuid:playlist_id>/', detail_playlist, name='detail_playlist'),
+    path('ubah_main/<uuid:playlist_id>/', ubah_main, name='ubah_main'),
+    path('ubah_playlist/<uuid:playlist_id>/', ubah_playlist, name='ubah_playlist'),
 
     # R PLAY SONG
     path('r_play_song_main', show_r_play_song_main, name='r_play_song_main'),
@@ -16,6 +19,6 @@ urlpatterns = [
     path('r_play_song_download_lagu', show_r_play_song_download_lagu, name='r_play_song_download_lagu'),
     path('r_play_song_tambah_lagu_clear', show_r_play_song_tambah_lagu_clear, name='r_play_song_tambah_lagu_clear'),
 
-    # R PLAY USER PLAYLIST  
+    # R PLAY USER PLAYLIST
     path('r_play_playlist_main', show_r_play_playlist, name='r_play_playlist_main'),
 ]
--- fitur_hijau/views.py
+++ fitur_hijau/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from django.db import connection
+from django.views.decorators.csrf import csrf_exempt
+import uuid, datetime
 
 # CRUD Kelola User Playlist
 def kelola_user_playlist_main(request):
@@ -7,8 +9,8 @@ def kelola_user_playlist_main(request):
     cursor.execute(
         """
         SET search_path to MARMUT;
-        SELECT judul, jumlah_lagu, total_durasi
-        FROM USER_PLAYLIST
+        SELECT judul, jumlah_lagu, total_durasi, id_playlist
+        FROM USER_PLAYLIST UP
         """
     )
 
@@ -18,7 +20,94 @@ def kelola_user_playlist_main(request):
 def tambah_lagu(request):
     return render(request, 'crud_kelola_playlist/form_tambah_lagu.html')
 
+@csrf_exempt
 def tambah_playlist(request):
+    if request.method == 'POST':
+        judul = request.POST.get('judul')
+        deskripsi = request.POST.get('deskripsi')
+        email_pembuat = request.COOKIES.get('user_email')
+        id_user_playlist = uuid.uuid4()
+        tanggal_dibuat = datetime.datetime.now()
+        id_playlist = uuid.uuid4()
+
+        cursor = connection.cursor()
+        cursor.execute( 
+            """
+            SET search_path to MARMUT;
+            INSERT INTO PLAYLIST(id)
+            VALUES (%s);
+
+            INSERT INTO USER_PLAYLIST(email_pembuat, id_user_playlist, judul, deskripsi, jumlah_lagu, tanggal_dibuat, id_playlist, total_durasi)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (id_playlist, email_pembuat, id_user_playlist, judul, deskripsi, 0, tanggal_dibuat, id_playlist, 0)
+        )
+        return redirect("fitur_hijau:crud_kelola_playlist_main")
+
+    return render(request, "crud_kelola_playlist/form_tambah_playlist.html")
+
+@csrf_exempt
+def hapus_playlist(request, playlist_id):
+    if request.method == 'POST':
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            SET search_path to MARMUT;
+            DELETE FROM USER_PLAYLIST WHERE id_playlist = %s;
+            DELETE FROM PLAYLIST WHERE id = %s;
+            """, [playlist_id, playlist_id]
+        )
+        return redirect("fitur_hijau:crud_kelola_playlist_main")
+
+    return render(request, "crud_kelola_playlist/kelola_user_playlist.html")
+
+def detail_playlist(request, playlist_id):
+    cursor = connection.cursor()
+    cursor.execute("""
+        SET search_path to MARMUT;
+        SELECT AKUN.nama, UP.judul, UP.deskripsi, UP.jumlah_lagu, UP.total_durasi, UP.tanggal_dibuat
+        FROM USER_PLAYLIST UP
+        JOIN AKUN ON AKUN.email = UP.email_pembuat
+        WHERE id_playlist = %s
+    """, [playlist_id])
+    playlist = cursor.fetchone()
+
+    if not playlist:
+        return render(request, 'error.html', {'message': 'Playlist not found'})
+    
+    playlist_details = {
+        'nama': playlist[0],
+        'judul': playlist[1],
+        'deskripsi': playlist[2],
+        'jumlah_lagu': playlist[3],
+        'total_durasi': playlist[4],
+        'tanggal_dibuat': playlist[5],
+    }
+
+    cursor.execute("""
+        SET search_path to MARMUT;
+        SELECT konten.judul AS judul, konten.durasi AS durasi, akun.nama AS oleh
+        FROM USER_PLAYLIST up
+        JOIN PLAYLIST_SONG ps ON up.id_playlist = ps.id_playlist
+        JOIN SONG s ON ps.id_song = s.id_konten
+        JOIN KONTEN konten ON s.id_konten = konten.id
+        JOIN ARTIST artis ON s.id_artist = artis.id
+        JOIN AKUN akun ON artis.email_akun = akun.email
+        WHERE up.id_playlist = %s
+    """, [playlist_id])
+    songs = cursor.fetchall()
+
+    return render(request, 'crud_kelola_playlist/detail.html', {
+        'playlist': playlist_details,
+        'songs': songs,
+    })
+
+def ubah_main(request, playlist_id):
+    context = {'playlist_id': playlist_id}
+    return render(request, 'crud_kelola_playlist/ubah_playlist.html', context)
+
+@csrf_exempt
+def ubah_playlist(request, playlist_id):
+    context = {'playlist_id': playlist_id}
     if request.method == 'POST':
         judul = request.POST.get('judul')
         deskripsi = request.POST.get('deskripsi')
@@ -27,16 +116,14 @@ def tambah_playlist(request):
         cursor.execute(
             """
             SET search_path to MARMUT;
-            INSERT INTO USER_PLAYLIST(judul, deskripsi)
-            VALUES (%s, %s)
-            """, (judul, deskripsi)
+            UPDATE USER_PLAYLIST
+            SET judul = %s, deskripsi = %s
+            WHERE id_playlist = %s
+            """, [judul, deskripsi, playlist_id]
         )
-        return redirect('fitur_hijau:')
-
-    return render(request, "crud_kelola_playlist/form_tambah_playlist.html")
-
-def show_crud_kelola_playlist_detail(request):
-    return render(request, "crud_kelola_playlist/detail.html")    
+        return redirect('fitur_hijau:crud_kelola_playlist_main')
+    
+    return render(request, 'crud_kelola_playlist/ubah_playlist.html', context)
 
 
 # R Play Song
